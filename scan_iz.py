2b1a03be4e8cee3c2e9b2fdece0c0711da2baaae
Thomas Egerer <hakke_007@gmx.de>
2021-05-30 12:34:57 +0200

    Honor optional start date
    Recently there's way too many open spots in the next two days
    
    Signed-off-by: Thomas Egerer <hakke_007@gmx.de>

--- scan_iz.py
+++ scan_iz.py
@@ -22,33 +22,46 @@ else:
 
 def send_telegram_message(auth, id, msg):
     uri = bot_url.format(auth = auth, id = id, msg = urlencode({'text' : msg}))
-    print(f'sending reuquest "{uri}"')
+    print(f'sending request "{uri}"')
     request = http.request('GET', uri)
 
 def p_or_ps(count):
     return ' freier Platz' if (1 == count) else ' freie Plätze'
 
-def in_limits(limits, name, count):
-    ret = ''
-    for key in limits.keys():
-        limit = limits[key]['limit']
-        if key in name and ((limit > 0 and count >= limit) or (limit == 0 and count > 0)):
-            t = time.time()
-            elem = limits[key]
-            if not 'timestamp' in elem or t > elem['timestamp']:
-                elem['timestamp'] = t + (elem['cooldown'] if 'cooldown' in elem else cool_down_time)
-                ret = str(count) + p_or_ps(count) + ' in ' + name + '\n'
+def get_total(items, date):
+    total = 0
+    for item in items:
+        if item['d'] >= date and item['c'] > 0:
+            total += item['c']
+    return total
+
+def get_text(name, items, date):
+    ret = name + ':\n'
+    for item in items:
+        if item['d'] >= date and item['c'] > 0:
+            ret += ('\t' + time.strftime('%d.%m.%Y', time.localtime(item['d'])) + ': ' + str(item['c']) + '\n')
     return ret
 
-def print_iz(name, items):
-    if items['val'] > 0:
-        print(name + ': ' + str(items['val']) + p_or_ps(items['val']))
-        x = json.loads(items['val_s'])
-        for item in x:
-            c = item['c']
-            if c > 0:
-                print('	' + time.strftime('%d.%m.%Y', time.localtime(item['d'])) + ': ' + str(c))
-        print()
+def print_iz(name, items, l):
+    for key in l.keys():
+        if key in name:
+            total = get_total(items, l[key]['start_date'])
+            if 0 < total:
+                print(get_text(name, items, l[key]['start_date']))
+
+def in_limits(name, items, l):
+    msg = None
+    for key in l.keys():
+        if key in name:
+            total = get_total(items, l[key]['start_date'])
+            limit = l[key]['limit']
+            if ((limit > 0 and total >= limit) or (limit == 0 and total > 0)):
+                t = time.time()
+                elem = l[key]
+                if not 'timestamp' in elem or t > elem['timestamp']:
+                    elem['timestamp'] = t + (elem['cooldown'] if 'cooldown' in elem else cool_down_time)
+                    msg = str(total) + p_or_ps(total) + ' in ' + name + '\n'
+    return msg
 
 def check_json(js, key, dict):
     for elem in dict:
@@ -57,7 +70,7 @@ def check_json(js, key, dict):
             return False
     return True
 
-def scan_iz(auth, id, l = None):
+def scan_iz(l, auth = None, id = None):
     request = http.request('GET', iz_url)
     try:
         js = json.loads(request.data.decode('utf-8'))
@@ -79,13 +92,16 @@ def scan_iz(auth, id, l = None):
         name = dict[key]['name']
         items = dict[key]['counteritems'][0]
 
-        if l:
-            msg = in_limits(l, name, items['val'])
-            if (msg):
-                print_iz(name, items)
-                tele_msg += msg
-        else:
-            print_iz(name, items)
+        if 0 < items['val']:
+            j = json.loads(items['val_s'])
+            if id and auth:
+                msg = in_limits(name, j, l)
+                if (msg is not None):
+                    tele_msg += msg
+            else:
+                msg = ''
+            if msg is not None:
+                print_iz(name, j, l)
 
     if tele_msg:
         print(time.asctime((time.gmtime(int(time.strftime("%s", time.gmtime()))+3*3600))), end = ": \n")
@@ -114,14 +130,24 @@ def main():
     parser.add_argument('--oneshot', '-1', action='store_true', help='print *all* vacant spots and exit, no telegram message')
     parser.add_argument('--recipient', '-r', action='store', help='telegram id of the recipient')
     parser.add_argument('--bot-auth', '-a', action='store', help='auth string of telegram bot')
+    parser.add_argument('--start-date', '-d', action='store', help='only consider vacant spots after this date, format dd.mm')
     args = parser.parse_args()
+
+    if args.start_date is None:
+        t = time.mktime(time.localtime())
+    else:
+        t = round(time.mktime(time.strptime(args.start_date + '2021', '%d.%m.%Y')))
+
+    for key in limits.keys():
+        if 'start_date' not in limits[key]:
+            limits[key]['start_date'] = t
 
     if False == args.oneshot and (not args.recipient or not args.bot_auth):
         print('non-oneshot mode requires telegram recipient and bot auth\n', file = sys.stderr)
         parser.print_help()
         exit(-1);
     elif True == args.oneshot:
-        scan_iz(None, None)
+        scan_iz(limits)
         exit(0)
 
     i = 0
@@ -130,7 +156,7 @@ def main():
         print('executing run #' + str(i), end = '\r')
         # if run without argument, the script prints all venues with vacant
         # vaccination spots and does not generate a telegram message
-        scan_iz(args.bot_auth, args.recipient, limits)
+        scan_iz(limits, args.bot_auth, args.recipient)
         time.sleep(sleep_interval)
 
 if __name__ == '__main__': main()
